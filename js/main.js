cda7a7f9ae68e62dc4892de05e1e49af86e9281c
viniciuscodc <viniciuscodc@gmail.com>
2021-04-11 05:18:00 -0300

    quem somos

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
+function backgroundeffect() {
+  let getImg = document.querySelectorAll("#serv-grid-img");
+  let getBackground = document.querySelectorAll(".background-effect");
 
-function backgroundeffect(){
-let getImg = document.querySelectorAll("#x");
-let getBackground = document.querySelectorAll(".background-effect");
-
-getImg.forEach((i,j) => {
-
- i.addEventListener("mouseenter", function () {
-    getBackground.forEach((i) =>{
+  getImg.forEach((i, j) => {
+    i.addEventListener("mouseenter", function () {
+      getBackground.forEach((i) => {
         getBackground[j].style.cssText = "opacity: 0.7";
+      });
     });
   });
-});
-getImg.forEach((i,j) => {
+  getImg.forEach((i, j) => {
     i.addEventListener("mouseleave", function () {
-        getBackground.forEach((i) =>{
-            getBackground[j].style.cssText = "opacity: 0";
-        });
+      getBackground.forEach((i) => {
+        getBackground[j].style.cssText = "opacity: 0";
       });
+    });
   });
 }
 
-backgroundeffect()
+function createGrid(){
+  const teamMembers = 22;
+  let getTeam = document.querySelector(".team");
+  for (let i = 0; i < teamMembers; i++) {
+    let newDiv = document.createElement("div");
+    getTeam.appendChild(newDiv);
+    newDiv.setAttribute("class","team-grid-img"+(i+1));
+    newDiv.style.gridArea = "team-img"+(i+1);
+    newDiv.style.backgroundImage = "url(../images/team" + (i+1) + ".jpg)";
+
+  }
+}
+
+
+
+
+backgroundeffect();
+createGrid();
